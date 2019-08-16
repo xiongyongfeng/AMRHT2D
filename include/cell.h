745c571a0f775729bbb4897495ce1f9ecc919fab
alvarojunio <45803071+alvarojunio@users.noreply.github.com>
2019-08-16 14:55:36 -0300

    Add files via upload

--- include/cell.h
+++ include/cell.h
@@ -8,25 +8,29 @@ using namespace std;
 class cell {
  private:
   int x, y;
-  int level;//0, 1, 2, 3, 4, ... from the courser to the finest
-  int index;
+  char kind [2]; // it could be bottom left (bl), bottom right (br), top left (tl), top right (tr)
+  char last_kind [2]; // it could be bottom left (bl), bottom right (br), top left (tl), top right (tr) before last split
+  int level;//0, 1, 2, 3, 4, ... from the courser to the finest  
   //double delta_x, delta_y;
   list <cell *>::iterator pointer_to_list;//ponteiro para a célula na lista de células da malha
   
  public:
   cell ();
-  //cell (int x, int y, int level, double delta_x, double delta_y);
-  cell (int x, int y, int level, int index);
+  //initially this->kind and this->last_kind is equal to kind (= bl)
+  cell (int x, int y, char kind [], int level);
+  //constructor with parameter that specify the kind cell just before a split
+  cell (int x, int y, char kind[], char last_kind[], int level);
   int get_cell_x();
   int get_cell_y();
+  char * get_cell_kind();
+  char * get_cell_last_kind();
   int get_cell_level();
-  int get_cell_index();
-  void put_cell_index(int ivalue);
   //double get_cell_delta_x();
   //double get_cell_delta_y();
   void set_cell_pointer_to_list(list<cell *>::iterator p);
   list<cell *>::iterator get_cell_pointer_to_list();
   cell ** split ();
   void print_cell ();
+  void set_cell_kind(char kind[]);
   
 };
--- include/mesh.h
+++ include/mesh.h
@@ -14,12 +14,10 @@ class mesh {
   hash_table * H;
   vector<list <cell *> *> * l;
   int number_of_levels;
-  int nxb;
-  int nyb;
   vector <int> * max_dimension_by_level;
  public:
   mesh();
-  mesh(dominio * D, int n_levels, int nx, int ny, vector <int> * max_dimension_by_level);
+  mesh(dominio * D, int n_levels, vector <int> * max_dimension_by_level);
   void insert(cell *);
   list <cell *>::iterator remove(cell *);
   cell * search (int, int, int);
@@ -36,37 +34,17 @@ class mesh {
   void right_neighbours (cell * c, list <cell *> * nb);
   void up_neighbours (cell * c, list <cell *> * nb);
   void down_neighbours (cell * c, list <cell *> * nb);
-
-  void left_neighbours_fd (cell * c, list <cell *> * nb);
-  void right_neighbours_fd (cell * c, list <cell *> * nb);
-  void up_neighbours_fd (cell * c, list <cell *> * nb);
-  void down_neighbours_fd (cell * c, list <cell *> * nb);
-  void insert_neighbour(cell * c, list <cell *> * nb);
-  
   list <cell *> * neighbours (cell * c);
-  list <cell *> * neighbours_fd (cell * c);
   
   void calculation_exact_function (double (* f) (double_cell * c));
-  void calculation_function (double (* f) (double x, double y), vector <double> * fvalue);
-  int counting_mesh_cell();
-  void triangular_mesh(int * ncellp, int * nelemp, int * narep, int * nvertp);
-  void triangular_mesh_refined(int * ncellp, int * nelemp, int * narep, int * nvertp);
-  int search_left_neighbours(cell * c);
-  int search_down_neighbours(cell * c);
-  int search_right_neighbours(cell * c);
-  int search_up_neighbours(cell * c);
-  int search_vertice(int i, int j, int n, int id, int l, vector <cell *> * vert);
-  void ordena_vertice(int nvert, vector <int> * vind, vector <float> * vx, vector <float> * vy);
-  int search_vertice_index(float x, float y, int nvert, vector <float> * vx, vector <float> * vy, vector <int> * vind);
-  int neighbours_all_cell();
-  void mesh_adress(int ncell, vector<cell *> * elem);
-  void create_matrix_df (vector<double> * IA, vector<int> * JA, vector<int> * II, int ncell, int ncellv, vector <cell*> * elem);
-  double normmax(vector <double> * x, int ncell);
-  double erron2(vector <double> * x, vector <double> * xa, int ncell);
-  void gs_csr(vector <double> * x, vector <double> * A, vector <int> * IA, vector <int> * JA, vector <double> * b, int ncell);
-  void print_matrix(vector <int> * IA, vector <int> * JA, vector <double> * A, int ncell, int ncellv);
-  void rhs_dirichlet_boundary_conditions (double (*f) (double x, double y), vector<double> * fvalue);
-  
-  void create_unstructured_mesh(double (* f) (double x, double y), double (* df)(double x, double y));
+  void create_unstructured_mesh(double (* f) (double x, double y, double t), double tempo);
   dominio * get_dominio();
+
+  //Recebe uma célula c e devolve uma lista de células irmãs de c somente se existirem. Caso contrário, devolve NULL. As células irmãs de c são c mais outras três células produzidas com o refinamento de uma célula.
+  vector <cell *> * siblings (cell * c);
+
+  //Recebe uma lista de células irmãs e agrupa todas elas. Recebe também o ponteiro para a célula na lista de células que está sendo percorrida (importante para atualizar iterador para a função que chama merge). Devolve um iterador para a próxima célula que não é nenhuma daquelas que foram agrupadas.
+  list <cell *>::iterator merge (vector <cell *> * V, cell * c);
+ 
+  
 };
