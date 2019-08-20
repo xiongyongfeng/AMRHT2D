02fdf60c81386415f72d3de1c56aa2edcae40d07
pccalegari <53314320+pccalegari@users.noreply.github.com>
2019-08-20 11:37:15 -0300

    Update cell.h

--- include/cell.h
+++ include/cell.h
@@ -12,12 +12,13 @@ class cell {
   char last_kind [2]; // it could be bottom left (bl), bottom right (br), top left (tl), top right (tr) before last split
   int level;//0, 1, 2, 3, 4, ... from the courser to the finest  
   //double delta_x, delta_y;
+  int index;
   list <cell *>::iterator pointer_to_list;//ponteiro para a célula na lista de células da malha
   
  public:
   cell ();
   //initially this->kind and this->last_kind is equal to kind (= bl)
-  cell (int x, int y, char kind [], int level);
+  cell (int x, int y, char kind [], int level, int index);
   //constructor with parameter that specify the kind cell just before a split
   cell (int x, int y, char kind[], char last_kind[], int level);
   int get_cell_x();
@@ -25,6 +26,8 @@ class cell {
   char * get_cell_kind();
   char * get_cell_last_kind();
   int get_cell_level();
+  int get_cell_index();
+  void put_cell_index(int ivalue);
   //double get_cell_delta_x();
   //double get_cell_delta_y();
   void set_cell_pointer_to_list(list<cell *>::iterator p);
