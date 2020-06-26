fd98b78822bfb90eea0144c33926a2997a9c350c
pccalegari <53314320+pccalegari@users.noreply.github.com>
2020-06-26 17:12:50 -0300

    Add files via upload

--- test/test_com_merge.cpp
+++ test/test_com_merge.cpp
@@ -15,13 +15,18 @@ double f (double x, double y) {
 }
 
 double df (double x, double y, double tempo) {
-  return -4 * PI * PI * f(tempo * x, tempo * y);
+  double xc, yc, r, phi, d;
+  xc = 0.5;
+  yc = 0.5;
+  r = 0.25;
+  d = sqrt((x-xc)*(x-xc) +(y-yc)*(y-yc));
+  phi = tanh((r-d)/(sqrt(2)*1.5*tempo));
+  //return -4 * PI * PI * f(tempo * x, tempo * y);
+    return(phi);
 }
 
 int main (){
   
-  cell * c;
-
   list <particle *> P;
 
   particle * p = new particle();
@@ -32,109 +37,32 @@ int main (){
 
   q->print_particle();
   
-  //int level = 0;
-  int number_of_levels = 4;
-  vector<int> * max_dimension_by_level;
-  //double delta_x, delta_y;
+  int number_of_levels = 6;
+  int nxb = 32;
+  int nyb = 32;
+
   dominio * D;
   
   double xbegin, ybegin, xend, yend;
-  xbegin = ybegin = -1.;
+  xbegin = ybegin = 0.;
   xend = yend = 1.;
   D = new dominio (xbegin, ybegin, xend, yend);
   
   mesh * M;
   
-  //delta_x = delta_y = 0.05;
-  max_dimension_by_level = new vector<int>;
-  
-  //srand(time(NULL));
-  srand(12345);
-  
-  /**********initiallize mesh*******************/
-  for (int i = 0; i < number_of_levels; i++)
-    max_dimension_by_level->push_back((32 * pow(2,i)) * (32 * pow(2,i)));
-  M = new mesh(D, number_of_levels, max_dimension_by_level);
+  /******create a base mesh BASE x BASE *********/
+  //you can find the value for BASE at mesh.h file 
+  M = new mesh(D, number_of_levels, nxb, nyb);
   /*********************************************/
-
-  char kind [2];
-  kind[0] = 'b'; kind[1] = 'l';
-  for (int y = 0; y < 32; y++)
-    for (int x = 0; x < 32; x++){
-      c = new cell(x, y, kind, 0);
-      M->insert(c);
-    }
     
-  /**************Refinement*************************/
-  //list <cell *> * l = M->get_list_cell_by_level (0);
-  //list <cell *>::iterator it = l->begin();
-  //int r;
-  //while (it != l->end()) {
-  //  if (/*1*/((*it)->get_cell_x() >= 10 && (*it)->get_cell_x () <= 21)/* r = rand() % 3 == 0 || r % 2 == 1*/) {
-  //   c = M->search((*it)->get_cell_x(), (*it)->get_cell_y(), (*it)->get_cell_level());
-  //    assert (c != NULL);
-  //    it = M->split_and_insert(c);
-  //  }
-  //  else
-  //    it++;
-  //}
-
-  //l = M->get_list_cell_by_level (1);
-  //it = l->begin();
-  //while (it != l->end()) {
-  //  if (/*1*//*r = rand() % 3 == 0 ||*/ rand() % 3 == 1) {
-  //    c = M->search((*it)->get_cell_x(), (*it)->get_cell_y(), (*it)->get_cell_level());
-  //    assert (c != NULL);
-  //    it = M->split_and_insert(c);
-  //  }
-  //  else
-  //    it++;
-  //}
-
-  //l = M->get_list_cell_by_level (2);
-  //it = l->begin();
-  //while (it != l->end()) {
-  //  if (/*1*//*r = rand() % 3 == 0 ||*/ rand() % 3 == 1) {
-  //    c = M->search((*it)->get_cell_x(), (*it)->get_cell_y(), (*it)->get_cell_level());
-  //    assert (c != NULL);
-  //    it = M->split_and_insert(c);
-  //  }
-  //  else
-  //    it++;
-  //}
-
-  //l = M->get_list_cell_by_level (3);
-  //it = l->begin();
-  //while (it != l->end()) {
-  //  if (/*1*//*rand() % 3 == 0 ||*/ rand() % 3 == 1) {
-  //    c = M->search((*it)->get_cell_x(), (*it)->get_cell_y(), (*it)->get_cell_level());
-  //    assert (c != NULL);
-  //    it = M->split_and_insert(c);
-  //  }
-  //  else
-  //    it++;
-  //}
-  
-  //l = M->get_list_cell_by_level (4);
-  //it = l->begin();
-  //while (it != l->end()) {
-  //  if (/*1*//*rand() % 3 == 0 ||*/ rand() % 3 == 1) {
-  //    c = M->search((*it)->get_cell_x(), (*it)->get_cell_y(), (*it)->get_cell_level());
-  //    assert (c != NULL);
-  //    it = M->split_and_insert(c);
-  //  }
-  //  else
-  //    it++;
-  //}
-  /******************************************/
-  
-  M->get_hash_table()->print_information();
+  //M->get_hash_table()->print_information();
 
   
   double dx, dy, xd, yd;
   list <cell *> * l;
   list <cell *>::iterator it;
   double tempo;
+  vector <cell *> * V;
 
   xbegin = M->get_dominio()->get_xbegin();
   ybegin = M->get_dominio()->get_ybegin();
@@ -142,83 +70,51 @@ int main (){
   yend = M->get_dominio()->get_yend();
   
   for (tempo = -1; tempo <= 0; tempo += 0.005){
-    //printf ("%lf\n", tempo);
     //refinement & merge
     for (int i = number_of_levels - 1; i >= 0 ; i--) {
       l = M->get_list_cell_by_level(i);
       
-      if (l->size() > 0){
-	it = l->begin();
-	//(*it)->print_cell();
-	//cout << endl;
-	dx = fabs(xend - xbegin) / (32 * pow(2, i));
-	dy = fabs(yend - ybegin) / (32 * pow(2, i));
-	//int contador = 0;
-	while (it != l->end()){
-	  
-	  xd = xbegin + ((*it)->get_cell_x() * dx);//(delta_0 / pow(2, i)));
-	  yd = ybegin + ((*it)->get_cell_y() * dy);//(delta_0 / pow(2, i)));
+      dx = fabs(xend - xbegin) / (nxb * pow(2, i));
+      dy = fabs(yend - ybegin) / (nyb * pow(2, i));
 
-	  assert(((*it)->get_cell_kind()[0] == 'b' || (*it)->get_cell_kind()[0] == 't') && ((*it)->get_cell_kind()[1] == 'l' || (*it)->get_cell_kind()[1] == 'r'));
-
-	  //refinement
-	  if ((df(xd + (dx / 2.), yd + (dy / 2.), tempo) <= 25 && df(xd + (dx / 2.), yd + (dy / 2.), tempo) >= 18) ||
-	      (df(xd + (dx / 2.), yd + (dy / 2.), tempo) >= -25 && df(xd + (dx / 2.), yd + (dy / 2.), tempo) <= -18)){
-	    if (i < number_of_levels - 1 /*se i é diferente do último nível, então pode refinar*/){
-	      //c = M->search((*it)->get_cell_x(), (*it)->get_cell_y(), (*it)->get_cell_level());
-	      //assert(c != NULL);
-	      //assert(c == *it);
-	      it = M->split_and_insert(*it);
-	    }
-	    else {
-	      it++;
-	    }
-	    //***
+      it = l->begin();
+      
+      while (it != l->end()){
+	
+	xd = xbegin + ((*it)->get_cell_x() * dx);
+	yd = ybegin + ((*it)->get_cell_y() * dy);
+	
+	//refinement
+	if ((df(xd + (dx / 2.), yd + (dy / 2.), tempo) <= 25 && df(xd + (dx / 2.), yd + (dy / 2.), tempo) >= 18) ||
+	    (df(xd + (dx / 2.), yd + (dy / 2.), tempo) >= -25 && df(xd + (dx / 2.), yd + (dy / 2.), tempo) <= -18)){
+	  if (i < number_of_levels - 1 /*se i não é o último nível, então pode refinar*/){
+	    it = M->split(*it);
 	  }
-	  else{
-	    //it++; //código sem merge
-	    assert(i == (*it)->get_cell_level());
-	    //Mais uma implementação do merge ***************************
-	    if (i > 0) {//se i é diferente do primeiro nível, então pode fazer merge
-	      vector <cell *> * V;
-	      V = M->siblings((*it));
-	      if (V != NULL){
-		//cout << V->size() << endl;
-		it = M->merge(V, *it);
-		V->clear();
-	      }
-	      else it++;
+	  else {
+	    it++;
+	  }
+	  //***
+	}
+	//merge
+	else{
+	  if (i > 0) {//se i não é o primeiro nível, então pode fazer merge
+	    V = M->siblings((*it));
+	    if (V != NULL){
+	      it = M->merge(V, *it);
+	      V->clear();
 	    }
 	    else it++;
 	  }
+	  else it++;
 	}
       }
     }
     M->create_unstructured_mesh(&df, tempo);
-    M->get_hash_table()->print_information();
+    //M->get_hash_table()->print_information();
   }
   
   M->create_unstructured_mesh(&df, tempo);
-  M->get_hash_table()->print_information();
+  //M->get_hash_table()->print_information();
   
-  
-  cell * cnew = new cell (47, 16, kind, 2);
-  
-  hash_table * Hnew = M->get_hash_table();
-
-  //Hnew->print_hash_table();
-
-  if (Hnew->search(47, 16, 2) != NULL) {
-
-    list <cell *> * lnew = M->neighbours (cnew);
-    
-    for (list <cell *>::iterator it = lnew->begin(); it != lnew->end(); it++) {
-      (*it)->print_cell();
-    }
-  }
-  else
-    {
-      cout << "There is no such cell!" << endl;
-    }
   return 0;
 }
---
+++ test/test_particles.cpp
@@ -0,0 +1,280 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include "mesh.h"
+#include "mls.h"
+#include "ode.h"
+
+#define PI 3.1415926535897
+#define Re 50.0
+#define Sc 10.
+#define Xsi 200.0 //200.0
+#define xc 0.
+#define yc 0.
+#define Ca 24.0
+#define rdens 1.18/2470.0
+
+using namespace std;
+
+
+double u (double x, double y, double t){
+  double r, vtc;
+  r = sqrt((x - xc)*(x - xc) + (y - yc)*(y - yc));
+  vtc = (Re*Sc/(2*PI*r))*(1.0 - exp(-(r*r)/(4.0*Sc*t)));
+  return ((y - yc)*vtc/r);
+  //return(-r*sin(t));
+}
+
+double v (double x, double y, double t){
+  double r, vtc;
+  r = sqrt((x - xc)*(x - xc) + (y - yc)*(y - yc));
+  vtc = (Re*Sc/(2*PI*r))*(1.0 - exp(-(r*r)/(4.0*Sc*t)));
+  return (-(x - xc)*vtc/r);
+  //return(r*cos(t));
+}
+
+double phi(double x, double y, double t){
+ 
+  //return (exp(-t)*cos(2*PI*x)*sin(2*PI*y));
+  //return (0.5*(1.0 + tanh(Xsi*(y - yc))));
+  return 1.0;
+}
+
+double f (double x, double y) { 
+  return cos(2 * PI * x) * sin(2 * PI * y);
+}
+
+double df (double x, double y, double t) {
+  return (-exp(-t)*cos(2*PI*x)*sin(2*PI*y));
+}
+
+int main (){
+  
+  list <particle *> P;
+  
+  int npart = 10000;
+  //particle * particle_bck;
+
+  int number_of_levels = 4;
+  int nxb = 32;
+  int nyb = 32;
+
+  dominio * D;
+  
+  double xbegin, ybegin, xend, yend;
+  xbegin = -0.5;
+  xend = 0.5;
+  ybegin = -0.5;
+  yend = 0.5;
+  D = new dominio (xbegin, ybegin, xend, yend);
+  
+  mesh * M;
+  
+  /******create a base mesh BASE x BASE *********/
+  //you can find the value for BASE at mesh.h file 
+  M = new mesh(D, number_of_levels, nxb, nyb);
+  /*********************************************/
+  
+  srand(123456);
+  double raio;
+  double xp, yp;//, up, vp, ufp, vfp, taup, nvelup;
+  
+
+  vector <double> vmax, xv, yv, uv, vv;
+  double dt; //dx, dy, dt;//, velu, velv;
+  double k1u, k1v, k2u, k2v, k3u, k3v, k4u, k4v;
+  list <cell *> * l;
+  list <cell *>::iterator it, itv, itb;
+  double tempo = 1.e-6, tf = 1.0e-2;
+  //list <cell *> * lv, ln;
+  //cell ** sv;
+  vector <cell *> *V;
+    
+  cell * cparticle, * cparticle_bck;
+ 
+  xbegin = M->get_dominio()->get_xbegin();
+  ybegin = M->get_dominio()->get_ybegin();
+  xend = M->get_dominio()->get_xend();
+  yend = M->get_dominio()->get_yend();
+
+  raio = fabs((xend - xbegin))/2.0 - 0.05;
+    
+  for(int k = 0; k < npart; k++){
+    particle * p = new particle();
+    if(rand()%2 == 0){
+      p->set_particle_x(raio*cos(rand())+xc);
+      p->set_particle_y(yc+0.001*sin(rand()));
+    }
+    else{
+      p->set_particle_x(raio*cos(rand())+xc);
+      p->set_particle_y(yc-0.001*sin(rand()));
+    }
+    p->set_particle_radius(20.0e-6);
+    xp = p->get_particle_x();
+    yp = p->get_particle_y();
+    p->set_particle_vx(u(xp,yp,tempo));
+    p->set_particle_vy(v(xp,yp,tempo));
+    
+    //refina a celula que contem a particula ate o nivel mais fino
+    for(int k = 0; k < number_of_levels - 1; k++){
+      cparticle = M->search_particle_cell(p, k);
+      if(cparticle != NULL){
+	M->split(cparticle);
+      }
+    }
+    //Neste ponto a particula esta no nivel mais fino
+    p->set_particle_level(number_of_levels - 1);
+    cparticle = M->search_particle_cell(p, p->get_particle_level());
+    cparticle->set_cell_with_particle(1);
+    /*p->print_particle();
+    lv = M->neighbours(cparticle);
+    for(itv = lv->begin(); itv != lv->end(); itv++){
+      (*itv)->print_cell();
+      if((*itv)->get_cell_level() != number_of_levels - 1){
+	ln.push_front(*itv);
+      }
+    }
+    while(ln.size() != 0){
+      itv = ln.begin();
+      ln.pop_front();
+      sv = M->split_return_new_cells(*itv);
+      
+      for(int k = 0; k < 4; k++){
+	if(sv[k]->get_cell_level() != number_of_levels - 1)
+	  ln.push_front(sv[k]);
+      }
+      }*/
+    
+    P.push_back(p);
+  }
+
+  int ct = 0;
+  M->initialize_var(&u, &v, &phi, tempo, tempo);
+  printf("%d %.8e\n", ct, tempo);
+  M->print_silo(ct, &P);
+  
+  double h = min(fabs(xend - xbegin) / (nxb * pow(2, number_of_levels-1)), fabs(yend - ybegin) / (nyb * pow(2, number_of_levels-1)));
+  vmax = M->max_propriedades();
+  dt = 0.5*h/max(vmax[0],vmax[1]);
+  
+  while(tempo < tf){
+    printf("%d %.8g\n", ct+1, tempo+dt);
+  
+    //Localiza particula e marca posicao da particulas e vizinhas
+    // -> "deve ser uma funcao" 
+    l = M->get_list_cell_by_level(number_of_levels - 1);
+    it = l->begin();
+    
+    while (it != l->end()){
+      (*it)->set_cell_with_particle(0);
+      it++;
+    }
+
+    for(list <particle *>::iterator itp = P.begin(); itp != P.end(); itp++){
+      
+      xp = (*itp)->get_particle_x();
+      yp = (*itp)->get_particle_y();
+      /*cparticle_bck = M->search_particle_cell((*itp), number_of_levels - 1);
+      cparticle_bck->set_cell_with_particle(1);
+      particle_bck = new particle(xp, yp, 0, 0, (*itp)->get_particle_level());
+      Pbck.push_back(particle_bck);*/
+      k1u = u(xp,yp,tempo);
+      k1v = v(xp,yp,tempo);
+      // RK1
+      //(*itp)->set_particle_x(rk1(dt, xp, k1u));
+      //(*itp)->set_particle_y(rk1(dt, yp, k1v));
+      // RK2
+      //k2u = u(xp + dt*k1u, yp + dt*k1v, tempo + dt);
+      //k2v = v(xp + dt*k1u, yp + dt*k1v, tempo + dt);
+      //(*itp)->set_particle_x(rk2(dt, xp, k1u, k2u));
+      //(*itp)->set_particle_y(rk2(dt, yp, k1v, k2v));
+      k2u = u(xp + 0.5*dt*k1u, yp + 0.5*dt*k1v, tempo + dt*0.5);
+      k2v = v(xp + 0.5*dt*k1u, yp + 0.5*dt*k1v, tempo + dt*0.5);
+      k3u = u(xp + 0.5*dt*k2u, yp + 0.5*dt*k2v, tempo + dt*0.5);
+      k3v = v(xp + 0.5*dt*k2u, yp + 0.5*dt*k2v, tempo + dt*0.5);
+      k4u = u(xp + dt*k3u, yp + dt*k3v, tempo + dt);
+      k4v = v(xp + dt*k3u, yp + dt*k3v, tempo + dt);
+      // RK4
+      (*itp)->set_particle_x(rk4(dt, xp, k1u, k2u, k3u, k4u));
+      (*itp)->set_particle_y(rk4(dt, yp, k1v, k2v, k3v, k4v));	  
+
+      cparticle = M->search_particle_cell((*itp), number_of_levels - 1);
+      if(cparticle == NULL){
+	/*for(int k = number_of_levels - 1; k > 0; k--){
+	  V = M->siblings((cparticle_bck));
+	  if (V != NULL){
+	    it = M->merge(V, cparticle_bck);
+	    V->clear();
+	  }
+	  cparticle_bck = M->search_particle_cell(particle_bck, k-1);
+	  }*/
+	      
+	for(int k = number_of_levels - 2; k >= 0; k--){
+	  cparticle = M->search_particle_cell((*itp), k);
+	  if(cparticle != NULL){
+	     break;
+	  }
+	}
+      }
+      if(cparticle->get_cell_level() != number_of_levels - 1){
+	//refina
+	for(int k = cparticle->get_cell_level(); k < number_of_levels - 1; k++){
+	  cparticle = M->search_particle_cell((*itp), k);
+	  if(cparticle != NULL){
+	    M->split(cparticle);
+	  }
+	}
+      }
+      //Neste ponto a particula esta no nivel mais fino
+      (*itp)->set_particle_level(number_of_levels - 1);
+      cparticle = M->search_particle_cell((*itp), (*itp)->get_particle_level());
+      cparticle->set_cell_with_particle(1);
+    }
+
+    // percorre lista de celulas do nivel mais fino
+    l = M->get_list_cell_by_level(number_of_levels - 1);
+    it = l->begin();
+    
+    while (it != l->end()){
+      V = M->siblings((*it));
+      if (V != NULL && V->at(0)->get_cell_with_particle() == 0 && V->at(1)->get_cell_with_particle() == 0 && V->at(2)->get_cell_with_particle() == 0 && V->at(3)->get_cell_with_particle() == 0){
+	int xv = V->at(0)->get_cell_x()/2;
+	int yv = V->at(0)->get_cell_y()/2;
+	it = M->merge(V, (*it));
+	cparticle_bck = M->search(xv, yv, number_of_levels - 2);  
+	V->clear();
+	for(int k = number_of_levels - 2; k > 0; k--){
+	  V = M->siblings((cparticle_bck));
+	  if (V != NULL){
+	    xv = V->at(0)->get_cell_x()/2;
+	    yv = V->at(0)->get_cell_y()/2;
+	    itv = M->merge(V, cparticle_bck);
+	    cparticle_bck = M->search(xv, yv, k-1);  
+	    V->clear();
+	  }
+	}
+      }
+      else
+	it++;
+    }
+    
+    ct++;
+    dt = min(dt, tf - tempo);
+    tempo += dt;
+    M->initialize_var(&u, &v, &phi, tempo, tempo);
+    //if(ct%100 == 0)
+    M->print_silo(ct, &P);
+    M->get_hash_table()->print_information();
+    vmax = M->max_propriedades();
+    dt = 0.5*h/max(vmax[0],vmax[1]);    
+    
+  }
+
+
+  
+  return 0;
+}
+
+
